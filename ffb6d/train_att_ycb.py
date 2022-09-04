58073e9c3c570e205bde25fd4529908d6d770862
Nachiket Desai <onenachiket@gmail.com>
2022-09-04 07:21:49 +0200

    t1

--- ffb6d/train_att_ycb.py
+++ ffb6d/train_att_ycb.py
@@ -15,7 +15,6 @@ import numpy as np
 import cv2
 import pickle as pkl
 from collections import namedtuple
-from cv2 import imshow, waitKey
 
 import torch
 import torch.optim as optim
@@ -26,22 +25,35 @@ from torch.optim.lr_scheduler import CyclicLR
 import torch.backends.cudnn as cudnn
 from tensorboardX import SummaryWriter
 
+#from common import Config, ConfigRandLA
 from common import Config, ConfigRandLA, ConfigTrans
+import datasets.ycb.ycb_dataset as dataset_desc
+from utils.pvn3d_eval_utils_kpls import TorchEval
+from utils.basic_utils import Basic_Utils
+
 import models.pytorch_utils as pt_utils
 #from models.ffb6d import FFB6D
 from models.attentionffb import AttFFB6D as FFB6D
-#from models.attffb_fuse import AttFFB6D as FFB6D
-from models.att_loss import OFLoss, FocalLoss
-#from models.loss import OFLoss, FocalLoss
-from utils.pvn3d_eval_utils_kpls import TorchEval
-from utils.basic_utils import Basic_Utils
-import datasets.linemod.linemod_dataset as dataset_desc
+from models.loss import OFLoss, FocalLoss
 
 from apex.parallel import DistributedDataParallel
 from apex.parallel import convert_syncbn_model
 from apex import amp
 from apex.multi_tensor_apply import multi_tensor_applier
 
+
+config = Config()
+bs_utils = Basic_Utils(config)
+writer = SummaryWriter(log_dir=config.log_traininfo_dir)
+
+rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
+resource.setrlimit(resource.RLIMIT_NOFILE, (30000, rlimit[1]))
+
+color_lst = [(0, 0, 0)]
+for i in range(config.n_objects):
+    col_mul = (255 * 255 * 255) // (i+1)
+    color = (col_mul//(255*255), (col_mul//255) % 255, col_mul % 255)
+    color_lst.append(color)
 
 
 parser = argparse.ArgumentParser(description="Arg parser")
@@ -79,14 +91,6 @@ parser.add_argument(
 parser.add_argument(
     "-eval_net", action='store_true', help="whether is to eval net."
 )
-parser.add_argument(
-    '--cls', type=str, default="ape",
-    help="Target object. (ape, benchvise, cam, can, cat, driller," +
-    "duck, eggbox, glue, holepuncher, iron, lamp, phone)"
-)
-parser.add_argument(
-    '--test_occ', action="store_true", help="To eval occlusion linemod or not."
-)
 parser.add_argument("-test", action="store_true")
 parser.add_argument("-test_pose", action="store_true")
 parser.add_argument("-test_gt", action="store_true")
@@ -101,39 +105,15 @@ parser.add_argument('-g', '--gpus', default=8, type=int,
                     help='number of gpus per node')
 parser.add_argument('-nr', '--nr', default=0, type=int,
                     help='ranking within the nodes')
-parser.add_argument('--epochs', default=2, type=int,
-                    metavar='N', help='number of total epochs to run')
 parser.add_argument('--gpu', type=str, default="0,1,2,3,4,5,6,7")
 parser.add_argument('--deterministic', action='store_true')
 parser.add_argument('--keep_batchnorm_fp32', default=True)
 parser.add_argument('--opt_level', default="O0", type=str,
                     help='opt level of apex mix presision trainig.')
+
 args = parser.parse_args()
 
 os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
-
-config = Config(ds_name='linemod', cls_type=args.cls)
-bs_utils = Basic_Utils(config)
-writer = SummaryWriter(log_dir=config.log_traininfo_dir)
-
-import pandas as pd
-val_metrics_df = pd.DataFrame()
-loss_rgbd_seg_col = []
-loss_kp_of_col = []
-loss_ctr_of_col = []
-loss_all_col = []
-loss_target_col = []
-acc_rgbd_col = []
-val_acc_col = []
-
-rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
-resource.setrlimit(resource.RLIMIT_NOFILE, (30000, rlimit[1]))
-
-color_lst = [(0, 0, 0)]
-for i in range(config.n_objects):
-    col_mul = (255 * 255 * 255) // (i+1)
-    color = (col_mul//(255*255), (col_mul//255) % 255, col_mul % 255)
-    color_lst.append(color)
 
 
 lr_clip = 1e-5
@@ -171,7 +151,7 @@ def checkpoint_state(model=None, optimizer=None, best_prec=None, epoch=None, it=
 
 
 def save_checkpoint(
-        state, is_best, filename="att_checkpoint", bestname="att_model_best",
+        state, is_best, filename="checkpoint", bestname="model_best",
         bestname_pure='attffb6d_best'
 ):
     filename = "{}.pth.tar".format(filename)
@@ -186,30 +166,29 @@ def load_checkpoint(model=None, optimizer=None, filename="checkpoint"):
 
     if os.path.isfile(filename):
         print("==> Loading from checkpoint '{}'".format(filename))
-        ck = torch.load(filename)
-        epoch = ck.get("epoch", 0)
-        it = ck.get("it", 0.0)
-        best_prec = ck.get("best_prec", None)
-        if model is not None and ck["model_state"] is not None:
-            ck_st = ck['model_state']
+        checkpoint = torch.load(filename)
+        epoch = checkpoint["epoch"]
+        it = checkpoint.get("it", 0.0)
+        best_prec = checkpoint["best_prec"]
+        if model is not None and checkpoint["model_state"] is not None:
+            ck_st = checkpoint['model_state']
             if 'module' in list(ck_st.keys())[0]:
                 tmp_ck_st = {}
                 for k, v in ck_st.items():
                     tmp_ck_st[k.replace("module.", "")] = v
                 ck_st = tmp_ck_st
             model.load_state_dict(ck_st)
-        if optimizer is not None and ck["optimizer_state"] is not None:
-            optimizer.load_state_dict(ck["optimizer_state"])
-        if ck.get("amp", None) is not None:
-            amp.load_state_dict(ck["amp"])
+        if optimizer is not None and checkpoint["optimizer_state"] is not None:
+            optimizer.load_state_dict(checkpoint["optimizer_state"])
+        amp.load_state_dict(checkpoint["amp"])
         print("==> Done")
         return it, epoch, best_prec
     else:
-        print("==> ck '{}' not found".format(filename))
+        print("==> Checkpoint '{}' not found".format(filename))
         return None
 
 
-def view_labels(rgb_chw, cld_cn, labels, K=config.intrinsic_matrix['linemod']):
+def view_labels(rgb_chw, cld_cn, labels, K=config.intrinsic_matrix['ycb_K1']):
     rgb_hwc = np.transpose(rgb_chw[0].numpy(), (1, 2, 0)).astype("uint8").copy()
     cld_nc = np.transpose(cld_cn.numpy(), (1, 0)).copy()
     p2ds = bs_utils.project_p3d(cld_nc, 1.0, K).astype(np.int32)
@@ -237,13 +216,12 @@ def model_fn_decorator(
         test_pose=False
     ):
         if finish_test:
-            teval.cal_lm_add(config.cls_id)
+            teval.cal_auc()
             return None
         if is_eval:
             model.eval()
         with torch.set_grad_enabled(not is_eval):
             cu_dt = {}
-            #print('Nachi says:', data.keys())
             # device = torch.device('cuda:{}'.format(args.local_rank))
             for key in data.keys():
                 if data[key].dtype in [np.float32, np.uint8]:
@@ -254,8 +232,7 @@ def model_fn_decorator(
                     cu_dt[key] = data[key].float().cuda()
                 elif data[key].dtype in [torch.int32, torch.int16]:
                     cu_dt[key] = data[key].long().cuda()
-            #print('Nachi says:', cu_dt.keys())
-            #print(cu_dt['choose'].size())
+
             end_points = model(cu_dt)
 
             labels = cu_dt['labels']
@@ -277,19 +254,6 @@ def model_fn_decorator(
             _, cls_rgbd = torch.max(end_points['pred_rgbd_segs'], 1)
             acc_rgbd = (cls_rgbd == labels).float().sum() / labels.numel()
 
-            if args.debug:
-                show_lb = view_labels(
-                    data['rgb'], data['cld_rgb_nrm'][0, :3, :], cls_rgbd
-                )
-                show_gt_lb = view_labels(
-                    data['rgb'], data['cld_rgb_nrm'][0, :3, :],
-                    cu_dt['labels'].squeeze()
-                )
-                imshow("pred_lb", show_lb)
-                imshow('gt_lb', show_gt_lb)
-                waitKey(0)
-
-
             loss_dict = {
                 'loss_rgbd_seg': loss_rgbd_seg.item(),
                 'loss_kp_of': loss_kp_of.item(),
@@ -307,18 +271,6 @@ def model_fn_decorator(
                 if args.local_rank == 0:
                     writer.add_scalars('loss', loss_dict, it)
                     writer.add_scalars('train_acc', acc_dict, it)
-                    #Nachi: making metric df
-                    acc_rgbd_col.append(acc_dict['acc_rgbd'])
-                    loss_rgbd_seg_col.append(loss_dict['loss_rgbd_seg'])
-                    loss_kp_of_col.append(loss_dict['loss_kp_of'])
-                    loss_ctr_of_col.append(loss_dict['loss_ctr_of'])
-                    loss_all_col.append(loss_dict['loss_all'])
-                    loss_target_col.append(loss_dict['loss_target'])
-
-                    #print('NACHI: Add Scalars sizes:')
-                    #print(loss_dict.keys(), loss_dict[list(loss_dict.keys())[0]])   #dict_keys(['loss_rgbd_seg', 'loss_kp_of', 'loss_ctr_of', 'loss_all', 'loss_target'])    #0.05776539817452431
-                    #print(acc_dict.keys(), acc_dict[list(acc_dict.keys())[0]])      #dict_keys(['acc_rgbd'])                                                                #0.9297655820846558
-
             if is_test and test_pose:
                 cld = cu_dt['cld_rgb_nrm'][:, :3, :].permute(0, 2, 1).contiguous()
 
@@ -328,9 +280,8 @@ def model_fn_decorator(
                         cld, cu_dt['rgb'], cls_rgbd, end_points['pred_ctr_ofs'],
                         cu_dt['ctr_targ_ofst'], labels, epoch, cu_dt['cls_ids'],
                         cu_dt['RTs'], end_points['pred_kp_ofs'],
-                        cu_dt['kp_3ds'], cu_dt['ctr_3ds'],
-                        ds='linemod', obj_id=config.cls_id,
-                        min_cnt=1, use_ctr_clus_flter=True, use_ctr=True,
+                        cu_dt['kp_3ds'], cu_dt['ctr_3ds'], min_cnt=1,
+                        use_ctr_clus_flter=True, use_ctr=True, ds='ycb'
                     )
                 else:
                     # test GT labels, keypoint and center point offset
@@ -339,10 +290,8 @@ def model_fn_decorator(
                     teval.eval_pose_parallel(
                         cld, cu_dt['rgb'], labels, gt_ctr_ofs,
                         cu_dt['ctr_targ_ofst'], labels, epoch, cu_dt['cls_ids'],
-                        cu_dt['RTs'], gt_kp_ofs,
-                        cu_dt['kp_3ds'], cu_dt['ctr_3ds'],
-                        ds='linemod', obj_id=config.cls_id,
-                        min_cnt=1, use_ctr_clus_flter=True, use_ctr=True
+                        cu_dt['RTs'], gt_kp_ofs, cu_dt['kp_3ds'], cu_dt['ctr_3ds'],
+                        min_cnt=1, use_ctr_clus_flter=True, use_ctr=True, ds='ycb'
                     )
 
         return (
@@ -439,31 +388,12 @@ class Trainer(object):
                 )
             seg_res_fn = 'seg_res'
             for k, v in acc_dict.items():
-                print(k,len(v), max(v), min(v), np.array(v).mean() * 100)
-                seg_res_fn += '_%s%.2f' % (k, np.array(v).mean() * 100)
+                seg_res_fn += '_%s%.2f' % (k, v)
             with open(os.path.join(config.log_eval_dir, seg_res_fn), 'w') as of:
                 for k, v in acc_dict.items():
                     print(k, v, file=of)
         if args.local_rank == 0:
-            #Nachi: removed next line, find a way to split the items inside and then save them
-            #print('NACHI:FINAL DIMS SCALAR')
-            #print(acc_dict.keys(), len(acc_dict[list(acc_dict.keys())[0]]), acc_dict[list(acc_dict.keys())[0]])
-            for sc in acc_dict['acc_rgbd']:
-                val_acc_col.append(sc)
-            #print('Nachi: Ouput lists size loss/acc: loss_rgbd_seg, loss_kp_of, loss_ctr_of, loss_all, loss_target, acc_rgbd, val_acc_rgbd: ')
-            #print(len(loss_rgbd_seg_col), loss_rgbd_seg_col[0])
-            #print(len(loss_kp_of_col), loss_kp_of_col[0])
-            #print(len(loss_ctr_of_col), loss_ctr_of_col[0])
-            #print(len(loss_all_col), loss_all_col[0])
-            #print(len(loss_target_col), loss_target_col[0])
-            #print(len(acc_rgbd_col), acc_rgbd_col[0])
-            #print(len(val_acc_col), val_acc_col[0])
-
-            for this_in, val in enumerate(acc_dict['acc_rgbd']):
-                writer.add_scalar(tag='val_acc', scalar_value=val, global_step=this_in)
-
-            acc_dict['acc_rgbd'] = sum(acc_dict['acc_rgbd'][-1041:]) / 1041
-            writer.add_scalars('val_acc_avg', acc_dict, it)
+            writer.add_scalars('val_acc', acc_dict, it)
 
         return total_loss / count, eval_dict
 
@@ -500,6 +430,7 @@ class Trainer(object):
         print("Totally train %d iters per gpu." % tot_iter)
 
         def is_to_eval(epoch, it):
+            # Eval after first 100 iters to test eval function.
             if it == 100:
                 return True, 1
             wid = tot_iter // clr_div
@@ -513,10 +444,9 @@ class Trainer(object):
         it = start_it
         _, eval_frequency = is_to_eval(0, it)
 
-        with tqdm.tqdm(range(config.n_total_epoch), desc="%s_epochs" % args.cls) as tbar, tqdm.tqdm(
+        with tqdm.tqdm(range(config.n_total_epoch), desc="epochs") as tbar, tqdm.tqdm(
             total=eval_frequency, leave=False, desc="train"
         ) as pbar:
-
             for epoch in tbar:
                 if epoch > config.n_total_epoch:
                     break
@@ -612,24 +542,24 @@ def train():
     torch.manual_seed(0)
 
     if not args.eval_net:
-        train_ds = dataset_desc.Dataset('train', cls_type=args.cls)
+        train_ds = dataset_desc.Dataset('train')
         train_sampler = torch.utils.data.distributed.DistributedSampler(train_ds)
         train_loader = torch.utils.data.DataLoader(
             train_ds, batch_size=config.mini_batch_size, shuffle=False,
             drop_last=True, num_workers=4, sampler=train_sampler, pin_memory=True
         )
 
-        val_ds = dataset_desc.Dataset('test', cls_type=args.cls)
+        val_ds = dataset_desc.Dataset('test')
         val_sampler = torch.utils.data.distributed.DistributedSampler(val_ds)
         val_loader = torch.utils.data.DataLoader(
             val_ds, batch_size=config.val_mini_batch_size, shuffle=False,
             drop_last=False, num_workers=4, sampler=val_sampler
         )
     else:
-        test_ds = dataset_desc.Dataset('test', cls_type=args.cls)
+        test_ds = dataset_desc.Dataset('test')
         test_loader = torch.utils.data.DataLoader(
             test_ds, batch_size=config.test_mini_batch_size, shuffle=False,
-            num_workers=10
+            num_workers=20
         )
 
     rndla_cfg = ConfigRandLA
@@ -657,14 +587,9 @@ def train():
 
     # load status from checkpoint
     if args.checkpoint is not None:
-        if args.eval_net:
-            checkpoint_status = load_checkpoint(
-                model, None, filename=args.checkpoint[:-8]
-            )
-        else:
-            checkpoint_status = load_checkpoint(
-                model, optimizer, filename=args.checkpoint[:-8]
-            )
+        checkpoint_status = load_checkpoint(
+            model, optimizer, filename=args.checkpoint[:-8]
+        )
         if checkpoint_status is not None:
             it, start_epoch, best_loss = checkpoint_status
         if args.eval_net:
@@ -675,7 +600,7 @@ def train():
             model, device_ids=[args.local_rank], output_device=args.local_rank,
             find_unused_parameters=True
         )
-        clr_div = 2
+        clr_div = 6
         lr_scheduler = CyclicLR(
             optimizer, base_lr=1e-5, max_lr=1e-3,
             cycle_momentum=False,
@@ -687,8 +612,7 @@ def train():
         lr_scheduler = None
 
     bnm_lmbd = lambda it: max(
-        args.bn_momentum
-        * args.bn_decay ** (int(it * config.mini_batch_size / args.decay_step)),
+        args.bn_momentum * args.bn_decay ** (int(it * config.mini_batch_size / args.decay_step)),
         bnm_clip,
     )
     bnm_scheduler = pt_utils.BNMomentumScheduler(
@@ -714,8 +638,8 @@ def train():
         model,
         model_fn,
         optimizer,
-        checkpoint_name=os.path.join(checkpoint_fd, "AttFFB6D_%s" % args.cls),
-        best_name=os.path.join(checkpoint_fd, "AttFFB6D_%s_best" % args.cls),
+        checkpoint_name=os.path.join(checkpoint_fd, "AttFFB6D"),
+        best_name=os.path.join(checkpoint_fd, "AttFFB6D_best"),
         lr_scheduler=lr_scheduler,
         bnm_scheduler=bnm_scheduler,
     )
